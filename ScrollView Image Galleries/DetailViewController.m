aec8e7d58e3fce21d66b8644c196d8945c4dedec
Alex Lee <x.lee@mail.utoronto.ca>
2017-06-12 11:02:04 -0700

    Initial detailed image view commit, with zooming enabled

---
+++ ScrollView Image Galleries/DetailViewController.m
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.m
+//  ScrollView Image Galleries
+//
+//  Created by Alex Lee on 2017-06-12.
+//  Copyright © 2017 Alex Lee. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scroll;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    self.scroll.maximumZoomScale = 5;
+    self.scroll.minimumZoomScale = 1;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView;
+{
+    return self.imageView;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
